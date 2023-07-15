2d4b01420e1878923076450dda161818c7f7f543
Hiro Kato <hirokikato1@gmail.com>
2023-07-15 14:46:39 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, firstLetters){
+    return drivers.filter((driver) => driver.toLowerCase().indexOf(firstLetters.toLowerCase()) === 0)
+    //indexOf() method returns the first index at which a given element can be found in an array or -1 if not present
+    //the second argument passed to indexOf() represents the start from index
+}
+
+function matchName(driversObj, stringName){
+    return driversObj.filter(driver => driver.name === stringName)
+}
